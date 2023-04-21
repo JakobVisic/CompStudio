7b00f3e7a925bc6b54b9207bced31e9df6cbd936
Jakob Visic <jakob.visic@gmail.com>
2023-04-21 10:50:25 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import './style.css'
 import ReactDOM from 'react-dom/client'
 import { Canvas,  } from '@react-three/fiber'
 import Experience from './Experience.jsx'
-import Popup from './Popup.jsx'
 import React, { useState } from 'react';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'))
